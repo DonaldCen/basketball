b6e95cd5b0d4170e2cf6ad8b4224de6882e8fdec
DonaldCen <1147656534@qq.com>
2020-05-12 00:52:06 +0800

    引入安全模块

---
+++ basketball-register/src/main/java/com/sports/basketballregister/configure/WebSecurityConfigure.java
@@ -0,0 +1,20 @@
+package com.sports.basketballregister.configure;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Description
+ * @Author <a href="mailto:cenyingqiang@wxchina.com">yingqiang.Cen</a>
+ * @Date 2020/5/12
+ * @Version 1.0.0
+ */
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
